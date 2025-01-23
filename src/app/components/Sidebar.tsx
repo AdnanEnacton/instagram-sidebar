8e980ddce211e27d5858ce839012f93d58ff1732
AdnanEnacton <adnan.enacton@gmail.com>
2025-01-23 16:04:40 +0530

    fix: desktop flex and active page bold font

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 import {
@@ -15,32 +16,13 @@ import {
   SvgThreadsIcon,
   SvgUserIcon,
 } from "../../../public/Icons/Icons";
+import { usePathname } from "next/navigation";
 
 const SideBarTopData = [
-  {
-    id: 1,
-    name: "Home",
-    icon: <SvgHomeIcon />,
-    path: "/",
-  },
-  {
-    id: 2,
-    name: "Search",
-    icon: <SvgSearchIcon />,
-    path: "/item/search",
-  },
-  {
-    id: 3,
-    name: "Explore",
-    icon: <SvgExploreIcon />,
-    path: "/item/explore",
-  },
-  {
-    id: 4,
-    name: "Reels",
-    icon: <SvgReelsIcon />,
-    path: "/item/reels",
-  },
+  { id: 1, name: "Home", icon: <SvgHomeIcon />, path: "/" },
+  { id: 2, name: "Search", icon: <SvgSearchIcon />, path: "/item/search" },
+  { id: 3, name: "Explore", icon: <SvgExploreIcon />, path: "/item/explore" },
+  { id: 4, name: "Reels", icon: <SvgReelsIcon />, path: "/item/reels" },
   {
     id: 5,
     name: "Messages",
@@ -53,18 +35,8 @@ const SideBarTopData = [
     icon: <SvgNotificationIcon />,
     path: "/item/notifications",
   },
-  {
-    id: 7,
-    name: "Create",
-    icon: <SvgCreateIcon />,
-    path: "/item/create",
-  },
-  {
-    id: 8,
-    name: "Profile",
-    icon: <SvgUserIcon />,
-    path: "/item/profile",
-  },
+  { id: 7, name: "Create", icon: <SvgCreateIcon />, path: "/item/create" },
+  { id: 8, name: "Profile", icon: <SvgUserIcon />, path: "/item/profile" },
 ];
 
 const SideBarBottomData = [
@@ -74,23 +46,16 @@ const SideBarBottomData = [
     icon: <SvgAiStudioIcon />,
     path: "/item/ai-studio",
   },
-  {
-    id: 2,
-    name: "Threads",
-    icon: <SvgThreadsIcon />,
-    path: "/item/threads",
-  },
-  {
-    id: 3,
-    name: "More",
-    icon: <SvgMenuIcon />,
-    path: "/item/more",
-  },
+  { id: 2, name: "Threads", icon: <SvgThreadsIcon />, path: "/item/threads" },
+  { id: 3, name: "More", icon: <SvgMenuIcon />, path: "/item/more" },
 ];
 
 function Sidebar() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
-    <div className="hidden  h-screen w-fit xl:min-w-[335px] px-3 pt-2 pb-5 md:flex flex-col border-r-2 border-[#262626] space-y-5">
+    <div className="hidden h-screen w-fit xl:min-w-[335px] px-3 pt-2 pb-5 md:flex flex-col border-r-2 border-[#262626] space-y-5">
       <Link href="/" className="p-4">
         <p className="xl:hidden">
           <SvgInstagramIcon />
@@ -103,7 +68,9 @@ function Sidebar() {
         <div>
           {SideBarTopData.map((item) => (
             <Link
-              className="flex items-center gap-4 p-3 my-1 justify-center xl:justify-start"
+              className={`flex items-center gap-4 p-3 my-1 justify-center xl:justify-start ${
+                pathname === item.path ? "font-bold" : ""
+              }`}
               href={item.path}
               key={item.id}
             >
@@ -115,7 +82,9 @@ function Sidebar() {
         <div>
           {SideBarBottomData.map((item) => (
             <Link
-              className="flex items-center justify-center xl:justify-start gap-4 p-3 my-1"
+              className={`flex items-center justify-center xl:justify-start gap-4 p-3 my-1 ${
+                pathname === item.path ? "font-bold" : ""
+              }`}
               href={item.path}
               key={item.id}
             >
